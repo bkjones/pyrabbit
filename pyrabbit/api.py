e8fbe404c820da633268fedd1d3cc2387f996211
Brian K. Jones <bkjones@gmail.com>
2011-06-01 16:34:20 -0400

    A lot of tests, more API coverage, altered code layout to separate API interface from HTTP details

--- pyrabbit/api.py
+++ pyrabbit/api.py
@@ -1,11 +1,198 @@
+import httplib2
+import json
+from collections import namedtuple
+import os
+try:
+    # Python 3.x
+    from urllib.parse import urlunparse
+except ImportError:
+    # Python 2.7
+    from urlparse import urlunparse
+
+class HTTPError(Exception):
+    """
+    An error response from the API server. This should be an
+    HTTP error of some kind (404, 500, etc).
+
+    """
+    def __init__(self, status=None, reason=None):
+        self.status = status
+        self.reason = reason
+        self.output = "%s - %s" % (self.status, self.reason)
+
+    def __str__(self):
+        return self.output
+
+class NetworkError(Exception):
+    pass
+
+class APIError(Exception):
+    pass
+
+class HTTPClient(object):
+    urls = {'overview': 'api/overview', 
+            'all_queues': 'api/queues',
+            'all_exchanges': 'api/exchanges',
+            'all_channels': 'api/channels',
+            'all_connections': 'api/connections',
+            'all_nodes': 'api/nodes',
+            'all_vhosts': 'api/vhosts',
+            'queues_by_vhost': 'api/queues/%s',
+            'exchanges_by_vhost': 'api/exchanges/%s',
+            'live_test': 'api/aliveness-test/%s'}
+
+    def __init__(self, server, uname, passwd):
+        self.client = httplib2.Http()
+        self.client.add_credentials(uname, passwd)
+        self.base_url = 'http://%s' % server
+
+    def decode_json_content(self, content):
+        str_ct = content.decode('utf8')
+        py_ct = json.loads(str_ct)
+        return py_ct
+
+    def do_call(self, path, reqtype):
+        try:
+            resp, content = self.client.request(path, reqtype)
+        except Exception as out:
+            # net-related exception types from httplib2 are unpredictable.
+            raise NetworkError("Error: %s %s" % (type(out), out))
+
+        if resp.status != 200:
+            raise HTTPError(resp.status, resp.reason)
+        else:
+            return resp, content
+
+    def is_alive(self, vhost='%2F'):
+        """
+        Uses the aliveness-test API call to determine if the
+        server is alive and the vhost is active. The broker (not this code)
+        creates a queue and then sends/consumes a message from it.
+
+        """
+        uri = os.path.join(self.base_url,
+                           HTTPClient.urls['live_test'] % vhost)
+
+        try:
+            resp, content = self.do_call(uri, 'GET')
+        except HTTPError as e:
+            if e.status == 404:
+                raise APIError("No vhost named '%s'" % vhost)
+            raise
+
+        if resp.status == 200:
+            return True
+        else:
+            return False
+
+    def get_overview(self):
+        """
+        /api/overview provides a bunch of miscellaneous data about the
+        broker instance itself.
+
+        """
+        resp, content = self.do_call(os.path.join(self.base_url,
+                                            HTTPClient.urls['overview']), 
+                                            'GET')
+
+        overview_data = self.decode_json_content(content)
+        return overview_data
+
+    def get_all_vhosts(self):
+        """
+        Get a list of all vhosts a broker knows about.
+
+        """
+        vhost = namedtuple('VHost', ['name'])
+        resp, content = self.do_call(os.path.join(self.base_url,
+                                            HTTPClient.urls['all_vhosts']),
+                                            'GET')
+
+        vhost_data = self.decode_json_content(content)
+        vhost_list = [vhost(**i) for i in vhost_data]
+        return vhost_list
+
+    def get_queues(self, vhost=None):
+        """
+        Get a list of all queues a broker knows about, or all queues in an
+        optionally-supplied vhost name.
+
+        """
+        if vhost:
+            path = HTTPClient.urls['queues_by_vhost'] % vhost
+        else:
+            path = HTTPClient.urls['all_queues']
+
+        resp, content = self.do_call(os.path.join(self.base_url, path), 'GET')
+        queues = self.decode_json_content(content)
+        return queues
+
+    def get_exchanges(self, vhost=None):
+        if vhost:
+            path = HTTPClient.urls['exchanges_by_vhost'] % vhost
+        else:
+            path = HTTPClient.urls['all_exchanges']
+
+        resp, content = self.do_call(os.path.join(self.base_url, path), 'GET')
+        exchanges = self.decode_json_content(content)
+        return exchanges
+            
+
 class Server(object):
     """
-    Abstraction of the API which acts as a proxy for 
-    requests going to a RabbitMQ server.
+    Abstraction of the RabbitMQ Management HTTP API.
 
-    Pretty much the entirety of the supported API for 
-    pyrabbit is accessed through this class.
-
+    HTTP calls are delegated to the  HTTPClient class for ease of testing,
+    cleanliness, separation of duty, flexibility, etc.
     """
-    def __init__(self, host):
+    def __init__(self, host, user, passwd):
+        """
+        Populates server attributes using passed-in parameters and 
+        the HTTP API's 'overview' information. It also instantiates
+        an httplib2 HTTP client and adds credentials based on 
+        passed-in initialization params. 
+
+        """
         self.host = host
+        self.client = HTTPClient(host, user, passwd)
+
+        # populate Server instance attrs from 'overview' data.
+        overview = self.client.get_overview()
+        
+        self.node = overview['node']
+        self.mgr_version = overview['management_version']
+
+        # returns a dict
+        self.queue_totals = overview['queue_totals']
+
+        # returns a list of dicts
+        self.listeners = overview['listeners']
+
+        self.stats_db_node = overview['statistics_db_node']
+        self.message_stats = overview['message_stats']
+        self.stats_level = overview['statistics_level']
+
+        return
+
+    def get_all_vhosts(self):
+        """
+        Returns a list of dicts resulting from a json.loads of the
+        server response without alteration.
+
+        """
+        vhosts = self.client.get_all_vhosts()
+        return vhosts
+
+    def get_queues(self, vhost=None):
+        """
+        Get all queues, or all queues in a vhost if vhost is not None.
+        Returns a list.
+
+        """
+        queues = self.client.get_queues(vhost)
+        return queues
+
+    def is_alive(self, vhost='%2F'):
+        return self.client.is_alive(vhost)
+
+
--- tests/test_pyrabbit.py
+++ tests/test_pyrabbit.py
@@ -1,13 +1,136 @@
-import unittest
+import json
+
+try:
+    #python 2.x
+    import unittest2 as unittest
+except ImportError:
+    #python 3.x
+    import unittest
+
 import sys
 sys.path.append('..')
 import pyrabbit
+import mock
+
+# used by Server init
+test_overview_dict = {'node': 'bar', 
+            'management_version': '2.4.1',
+            'queue_totals': 'rrrr',
+            'listeners': 'ssss',
+            'statistics_db_node': 'tttt',
+            'message_stats': 'uuuu',
+            'statistics_level': 'vvvv'}
+
+test_vhosts_dict = [{'name': '/'}]
+
+test_q_all = json.loads('[{"memory":12248,"messages":0,"consumer_details":[],"idle_since":"2011-5-27 15:17:35","exclusive_consumer_pid":"","exclusive_consumer_tag":"","messages_ready":0,"messages_unacknowledged":0,"messages":0,"consumers":0,"backing_queue_status":{"q1":0,"q2":0,"delta":["delta","undefined",0,"undefined"],"q3":0,"q4":0,"len":0,"pending_acks":0,"outstanding_txns":0,"target_ram_count":"infinity","ram_msg_count":0,"ram_ack_count":0,"ram_index_count":0,"next_seq_id":0,"persistent_count":0,"avg_ingress_rate":0.0,"avg_egress_rate":0.0,"avg_ack_ingress_rate":0.0,"avg_ack_egress_rate":0.0},"name":"testq","vhost":"/","durable":true,"auto_delete":false,"owner_pid":"none","arguments":{},"pid":"<rabbit@newhotness.3.225.0>","node":"rabbit@newhotness"}]')
+
+class TestHTTPClient(unittest.TestCase):
+    """
+    Except for the init test, these are largely functional tests that
+    require a RabbitMQ management API to be available on localhost:55672
+
+    """
+    def setUp(self):
+        self.c = pyrabbit.api.HTTPClient('localhost:55672', 'guest', 'guest')
+
+    def test_client_init(self):
+        c = pyrabbit.api.HTTPClient('localhost:55672', 'guest', 'guest')
+        self.assertIsInstance(c, pyrabbit.api.HTTPClient)
+
+    def test_is_alive(self):
+        self.assertTrue(self.c.is_alive())
+
+    def test_is_not_alive(self):
+        """
+        If your vhost isn't found, RabbitMQ throws a 404. This is mapped to
+        a more readable exception message in HTTPClient saying the vhost
+        doesn't exist.
+
+        """
+        with self.assertRaises(pyrabbit.api.APIError):
+            self.c.is_alive('somenonexistentvhost')
+
+    def test_overview_500(self):
+        """
+        Insures that if the broker is down, the pyrabbit.api.NetworkError gets
+        raised.
+
+        """
+        c = pyrabbit.api.HTTPClient('webstatuscodes.appspot.com/500',
+                                    'guest', 'guest')
+        with self.assertRaises(pyrabbit.api.HTTPError) as ctx:
+            c.get_overview()
+
+        self.assertEqual(ctx.exception.status, 500)
+
+    def test_get_overview(self):
+        overview = self.c.get_overview()
+        self.assertIsInstance(overview, dict)
+
+    def test_get_all_vhosts(self):
+        vhosts = self.c.get_all_vhosts()
+        self.assertIsInstance(vhosts, list)
+
+    def test_get_all_queues(self):
+        queues = self.c.get_queues()
+        self.assertIsInstance(queues, list)
+
+    def test_get_queues_for_vhost(self):
+        queues = self.c.get_queues('testvhost')
+        self.assertIsInstance(queues, list)
+
+    def test_get_all_exchanges(self):
+        xchs = self.c.get_exchanges()
+        self.assertIsInstance(xchs, list)
+
+    def test_get_exchanges_by_vhost(self):
+        xchs = self.c.get_exchanges('testvhost')
+        self.assertIsInstance(xchs, list)
+
 
 class TestServer(unittest.TestCase):
-    def test_server_init(self):
-        srvr = pyrabbit.api.Server('localhost')
-        self.assertIsInstance(srvr, pyrabbit.api.Server)
-        self.assertEqual(srvr.host, 'localhost')
+    def test_server_init_200(self):
+        with mock.patch('pyrabbit.api.HTTPClient', spec=True) as httpmock:
+            inst = httpmock.return_value
+            inst.get_overview.return_value = test_overview_dict
+
+            srvr = pyrabbit.api.Server('localhost:55672', 'guest', 'guest')
+
+            self.assertIsInstance(srvr, pyrabbit.api.Server)
+            self.assertTrue(all(x in srvr.__dict__.values() for x in \
+                                        test_overview_dict.values()))
+            self.assertEqual(srvr.host, 'localhost:55672')
+
+    def test_server_is_alive_default_vhost(self):
+        with mock.patch('pyrabbit.api.HTTPClient', spec=True) as httpmock:
+            httpmock.return_value.get_overview.return_value = test_overview_dict
+            httpmock.return_value.is_alive.return_value = True
+
+            srvr = pyrabbit.api.Server('localhost:55672', 'guest', 'guest')
+
+            self.assertTrue(srvr.is_alive())
+
+    def test_get_vhosts_200(self):
+        with mock.patch('pyrabbit.api.HTTPClient', spec=True) as httpmock:
+            httpmock.return_value.get_overview.return_value = test_overview_dict
+            httpmock.return_value.get_all_vhosts.return_value = test_vhosts_dict
+
+            srvr = pyrabbit.api.Server('localhost:55672', 'guest', 'guest')
+            vhosts = srvr.get_all_vhosts()
+
+            self.assertIsInstance(vhosts, list)
+
+    def test_get_all_queues(self):
+        with mock.patch('pyrabbit.api.HTTPClient', spec=True) as httpmock:
+            httpmock.return_value.get_overview.return_value = test_overview_dict
+            httpmock.return_value.get_queues.return_value = test_q_all
+
+            srvr = pyrabbit.api.Server('localhost:55672', 'guest', 'guest')
+            queues = srvr.get_queues()
+
+            self.assertIsInstance(queues, list)
+
 
 class TestExchange(unittest.TestCase):
     def test_exch_init(self):
@@ -15,6 +138,7 @@ class TestExchange(unittest.TestCase):
         self.assertIsInstance(xch, pyrabbit.exchanges.Exchange)
         self.assertEqual(xch.name, 'test')
     
+
 class TestQueue(unittest.TestCase):
     def test_queue_init(self):
         qu = pyrabbit.queues.Queue('testq')
