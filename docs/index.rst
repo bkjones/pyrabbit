10bc8b1876a3414fee562c64265f4dfe3dcbc906
Brian K. Jones <bkjones@gmail.com>
2011-09-21 09:26:38 -0400

    Sphinx documentation. It's all included right now. I'd like to figure out the right way to distribute these docs without making my dev workflow onerous. Clues hereby solicited.

---
+++ docs/index.rst
@@ -0,0 +1,23 @@
+.. PyRabbit documentation master file, created by
+   sphinx-quickstart on Tue Sep 20 22:48:33 2011.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to PyRabbit's documentation!
+====================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+.. automodule:: pyrabbit.api
+    :members:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- pyrabbit/api.py
+++ pyrabbit/api.py
@@ -26,6 +26,10 @@ class Server(object):
 
     def __init__(self, host, user, passwd):
         """
+        :param string host: string of the form 'host:port'
+        :param string user: username used to authenticate to the API.
+        :param string passwd: password used to authenticate to the API.
+
         Populates server attributes using passed-in parameters and 
         the HTTP API's 'overview' information. It also instantiates
         an httplib2 HTTP client and adds credentia    ls
@@ -60,10 +64,13 @@ class Server(object):
 
     def get_overview(self):
         """
+        :rtype: dict
+
         Data in the 'overview' depends on the privileges of the creds used,
         but typically contains information about the management plugin version,
         some high-level message stats, and aggregate queue totals. Admin-level
         creds gets you information about the cluster node, listeners, etc.
+
 
         """
         overview = self.client.get_overview()
@@ -72,7 +79,10 @@ class Server(object):
     @needs_admin_privs
     def get_users(self):
         """
-        Returns a list of dictionaries.
+        :returns: a list of dictionaries, each representing a user. This 
+            method is decorated with '@needs_admin_privs', and will raise
+            an error if the credentials used to set up the broker connection 
+            do not have admin privileges.
 
         """
         users = self.client.get_users()
@@ -80,12 +90,8 @@ class Server(object):
 
     def get_all_vhosts(self):
         """
-        Returns a list of namedtuples that act and are in all ways
-        equivalent to instances of type VHost. Since all operations on
-        these objects are done via the API provided by this Server class,
-        VHosts are just bags of vhost attributes that are addressable using
-        cleaner, more meaningful syntax than objects
-        (e.g. 'v.name' instead of 'v[0]')
+        :returns: a list of dicts, each dict representing a vhost 
+            on the broker. 
 
         """
         vhosts = self.client.get_all_vhosts()
@@ -96,6 +102,11 @@ class Server(object):
         Get all queues, or all queues in a vhost if vhost is not None.
         Returns a list.
 
+        :param string vhost: The virtual host to list queues for. If This is None
+                   (the default), all queues for the broker instance are returned. 
+        :returns: A list of dicts, each representing a queue.
+        :rtype: list of dicts
+
         """
         queues = self.client.get_queues(vhost)
         return queues
@@ -104,12 +115,30 @@ class Server(object):
         """
         Get a single queue, which requires both vhost and name.
 
+        :param string vhost: The virtual host for the queue being requested. 
+            If the vhost is '/', note that it will be translated to '%2F' to 
+            conform to URL encoding requirements. 
+        :param string name: The name of the queue being requested.
+        :returns: A dictionary of queue properties. 
+        :rtype: dict
+
         """
         vhost = '%2F' if vhost == '/' else vhost
         q = self.client.get_queue(vhost, name)
         return q
 
     def get_queue_depth(self, vhost, name):
+        """
+        Get the number of messages currently in a queue. This is a convenience
+         function that just calls :meth:`Server.get_queue` and pulls out/returns the 'messages' 
+         field from the dictionary it returns. 
+
+        :param string vhost: The vhost of the queue being queried.
+        :param string name: The name of the queue to query. 
+        :returns: Number of messages in the queue
+        :rtype: integer
+
+        """
         vhost = '%2F' if vhost == '/' else vhost
         q = self.get_queue(vhost, name)
         depth = q['messages']
@@ -117,11 +146,10 @@ class Server(object):
 
     def purge_queues(self, queues):
         """
-        The queues parameter should be a list of tuples of the format:
+        Purge all messages from one or more queues.
 
-        ('name', 'vhost')
-
-        If 'vhost' == '/', it'll be changed to '%2F'
+        :param list queues: A list of ('qname', 'vhost') tuples.
+        :returns: True on success
 
         """
         for name, vhost in queues:
@@ -130,12 +158,28 @@ class Server(object):
         return True
 
     def purge_queue(self, vhost, name):
+        """
+        Purge all messages from a single queue. This is a convenience method
+        so you aren't forced to supply a list containing a single tuple to 
+        the purge_queues method. 
+
+        :param string vhost: The vhost of the queue being purged.
+        :param string name: The name of the queue being purged.
+        :rtype: None
+
+        """
         vhost = '%2F' if vhost == '/' else vhost
         self.client.purge_queue(vhost, name)
+        return 
 
     def get_connections(self, name=None):
         """
-        Returns a list of one or more dictionaries
+        Returns a list of one or more dictionaries, each holding 
+        attributes of a connection to the broker. If 'name' is given and
+        not None, returns a dictionary for a named connection. 
+
+        :param string name: The name of a connection to query for. 
+        :returns: A list of dictionaries, each representing a connection.
 
         """
         connections = self.client.get_connections(name)
@@ -143,7 +187,8 @@ class Server(object):
 
     def get_exchanges(self, vhost=None):
         """
-        Returns a list of dictionaries.
+        :param string vhost: The vhost to list exchanges from.
+        :returns: a list of dictionaries.
 
         """
         xchs = self.client.get_exchanges(vhost)
@@ -151,7 +196,10 @@ class Server(object):
 
     def get_exchange(self, vhost, xname):
         """
-        Returns a single exchange namedtuple subclass.
+        :param string vhost: Name of the vhost the exchange belongs to.
+        :param string xname: Name of the exchange to query for. 
+        :returns: A dictionary w/ the exchange's attributes.
+        :rtype: dict
 
         """
         xch = self.client.get_exchange(vhost, xname)
